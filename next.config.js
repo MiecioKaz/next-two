2d85069b1dd011da65c625596d3a7d83ea1d78e7
Kaz Piekos <python_js@icloud.com>
2023-06-15 11:24:32 +0100

    delete unneeded files

--- next.config.js
+++ next.config.js
@@ -14,5 +14,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// "https://firebasestorage.googleapis.com/v0/b/polpets.appspot.com/o/relocate%2Fdog%2FqIv9JBm75ZOrQ4psu2am1CP5OQi2%2Fdoggy4.jpg?alt=media&token=a0004091-911e-4eb1-8698-051796cb9f76"
